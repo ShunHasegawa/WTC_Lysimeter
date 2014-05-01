de07c259ec110ea61472c84f205eb8e95c0992af
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-01 20:19:07 +1000

    functions to create and save summary table are copied from WTC_minerlisation

--- R/functions.R
+++ R/functions.R
@@ -99,3 +99,43 @@ cmbn.fls <- function(file){
   return(pr.cst)
 }
 
+##########################
+# Create a summary table #
+##########################
+CreateTable <- function(dataset, fac){
+  a <- dataset[c("date", fac, "value")] #extract required columns
+  colnames(a) <- c("date","variable","value") #change column names for cast
+  means <- cast(a, date~variable, mean, na.rm = TRUE) 
+  ses <- cast(a,date~variable,function(x) ci(x,na.rm=TRUE)[4])
+  colnames(ses)[2:ncol(ses)] <- paste(colnames(ses)[2:ncol(ses)],"SE",sep=".")
+  samples <- cast(a,date~variable,function(x) sum(!is.na(x))) #sample size
+  colnames(samples)[2:ncol(samples)] <- paste(colnames(samples)[2:ncol(samples)],"N",sep=".")
+  mer <- Reduce(function(...) merge(..., by = "date"), list(means, ses, samples)) #merge datasets
+  mer <- mer[,c(1, order(names(mer)[-grep("date|N", names(mer))])+1, grep("N", names(mer)))] #re-order columns
+  mer$date <- as.character(mer$date) # date is turned into character for knitr output 
+  return(mer)
+}
+
+#function which creates excel worksheets
+crSheet <- function(sheetname, dataset){
+  #create sheet
+  sheet <- createSheet(wb, sheetName = sheetname)
+  
+  #add data to the sheet
+  addDataFrame(dataset, sheet, showNA = TRUE, row.names = FALSE, startRow = 2)
+  
+  #title of the sheet
+  addDataFrame(t(c(sheetname, "unit=mg DrySoil(kg)^(-1) day^(-1)")), sheet, startRow = 1, row.names = FALSE, col.names = FALSE)
+}
+
+############################
+# make a summary dataframe #
+############################
+Crt_SmryDF <- function(data, val = "value"){
+  x <- data[ ,val]
+  Mean <- mean(x, na.rm = TRUE)
+  SE <- ci(x, na.rm = TRUE)[[4]]
+  N  <- sum(!is.na(x))
+  data.frame(Mean, SE, N)
+}
+
