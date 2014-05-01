9c2b4e1a203dd171ff04e17bd190d9cab626b4a0
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-01 15:48:15 +1000

    processing toc row data

---
+++ R/ProcessTOC.R
@@ -0,0 +1,69 @@
+rm(list = ls(all = TRUE))
+
+library(car)
+
+
+fls <- dir("Data//TOC", full.names= TRUE)
+
+rowdf <- ldply(fls, function(x) read.table( x, skip = 11, fill = TRUE, sep = "\t", header = TRUE, 
+                                         colClasses = c("Sample.ID" = "character")))
+
+# remove standard & control
+unique(rowdf$Type)
+Ndf <- subset(rowdf, Type == "Unknown")
+
+################################
+# Inspect and organise dataset #
+################################
+
+## Sample.ID ##
+unique(Ndf$Sample.ID)
+  # same samples were rerun becase their TN were higher than the topstandard
+
+# check rerun samples
+Ndf[grep("RERUN", Ndf$Sample.ID), ]
+  # 24-May-2013 2.s.1, 4.s.2, 10.s.1 were measured twice
+subset(Ndf, Sample.ID %in% c("2.S.1", "4.S.2", "10.S.1") & Sample.Name == "24-May-2013")
+  # They were pretty much same as the original values so just use the original values and remove rerun smaples
+
+# remove rerun samples
+Ndf <- Ndf[-grep("RERUN", Ndf$Sample.ID), ]
+
+
+## Sample.Name ##
+unique(Ndf$Sample.Name)
+ # There is "Untitled"
+Ndf[Ndf$Sample.Name == "Untitled", ]
+  # This is from 28-Jun-2013 casue it wasn't measured properly first time
+
+subset(Ndf, Sample.Name %in% c("28-Jun-2013", "Untitled") & Sample.ID == "12.D.1")
+  # also remove the 1st measurement for this
+  # and also fix "Untitled"
+
+Ndf <- Ndf[-which(Ndf$Sample.Name == "28-Jun-2013" &  Ndf$Sample.ID == "12.D.1"), ]
+Ndf$Sample.Name[which(Ndf$Sample.Name == "Untitled")] <- "28-Jun-2013"
+
+# check if these are fixed properly
+subset(Ndf, Sample.Name %in% c("28-Jun-2013", "Untitled") & Sample.ID == "12.D.1")
+
+unique(Ndf$Sample.Name)
+  #  need to replace 8-Oct-13 with 8-Oct-2013
+Ndf$Sample.Name <- recode(Ndf$Sample.Name, "'8-Oct-13' =  '08-Oct-2013'")
+unique(Ndf$Sample.Name)
+
+# turn Sample.Name into date
+Ndf$date <- as.Date(dmy(Ndf$Sample.Name))
+
+####################################
+# Finalise the shape of data frame #
+####################################
+# create ring, depth factor
+a <- ldply(strsplit(Ndf$Sample.ID, split = "[.]"))
+colnames(a) <- c("chamber", "depth", "location")
+
+# merge
+Ndf <- cbind(Ndf, a)
+
+# extract required coluns
+names(Ndf)
+tocDF <- Ndf[c("Result.TOC.", "Result.TC.", "Result.IC.", "Result.TN.", "date", "chamber", "depth", "location")]
