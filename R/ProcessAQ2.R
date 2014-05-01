f6aa957bc542183bdf5824c9ede981d42a22f8c6
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-01 14:02:14 +1000

    new files added to NeedToCorrectNO folder

--- R/ProcessAQ2.R
+++ R/ProcessAQ2.R
@@ -21,17 +21,21 @@ l_ply(pfiles, function(x) write.csv(CrrctNgtv(filename = x),
 ##############
 nofiles <- dir("Data//AQ2//NeedToCorrectNO")
 
-l_ply(nofiles[c(1,3)], function(x) 
+ccvList <- nofiles[-grep("19DEC2013", nofiles)]
+
+l_ply(ccvList, function(x) 
   write.csv(Crrtct.ccv.df(x), 
             paste("Data//AQ2//ReadyToProcess/", "CCV_Corrected_", x, sep = ""),
             row.names = TRUE))
 
 
-Dec2013 <- read.csv("Data//AQ2//NeedToCorrectNO//WTC.Lysimeter_19DEC2014_NO_x4.dilution.csv")
+Dec2013 <- read.csv("Data//AQ2//NeedToCorrectNO//WTC.Lysimeter_19DEC2013_NO_x4.dilution.csv")
   # add dilution factor of 4
 Dec2013$Result <- Dec2013$Result * 4
 
-write.csv(Dec2013, paste("Data//AQ2//ReadyToProcess/", "DilutionCorrected_", nofiles[2], sep = ""),
+write.csv(Dec2013, paste("Data//AQ2//ReadyToProcess/", "DilutionCorrected_", 
+                         "WTC.Lysimeter_19DEC2013_NO_x4.dilution.csv", 
+                         sep = ""),
                          row.names = TRUE)
 
 
