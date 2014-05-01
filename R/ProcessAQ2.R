b4954cf3351e1563850eab18256b1227877c2536
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-01 12:38:53 +1000

    No3_15 is corrected

--- R/ProcessAQ2.R
+++ R/ProcessAQ2.R
@@ -4,18 +4,34 @@ library(plyr)
 library(lubridate)
 library(reshape)
 
+source("R/functions.R")
+
 #############
 # Correct P #
 #############
 # correct negative values
-CrrctNgtv <- function(filename){
-  d <- read.csv(paste("Data//AQ2//NeedToCorrectP/", filename, sep = ""))
-  if(any(d$Result < 0)) d$Result <- d$Result - min(d$Result)
-  return(d)
-}
-
 pfiles <- dir("Data//AQ2//NeedToCorrectP")
 
 l_ply(pfiles, function(x) write.csv(CrrctNgtv(filename = x), 
             paste("Data//AQ2//ReadyToProcess/", "NgtvCorrected_", x, sep = ""),
             row.names = TRUE))
+
+##############
+# Correct NO #
+##############
+nofiles <- dir("Data//AQ2//NeedToCorrectNO")
+
+l_ply(nofiles[c(1,3)], function(x) 
+  write.csv(Crrtct.ccv.df(x), 
+            paste("Data//AQ2//ReadyToProcess/", "CCV_Corrected_", x, sep = ""),
+            row.names = TRUE))
+
+
+Dec2013 <- read.csv("Data//AQ2//NeedToCorrectNO//WTC.Lysimeter_19DEC2014_NO_x4.dilution.csv")
+  # add dilution factor of 4
+Dec2013$Result <- Dec2013$Result * 4
+
+write.csv(Dec2013, paste("Data//AQ2//ReadyToProcess/", "DilutionCorrected_", nofiles[2], sep = ""),
+                         row.names = TRUE)
+
+
