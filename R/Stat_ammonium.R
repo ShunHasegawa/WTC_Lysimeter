f0cb5ce82fbc07f6c0e09fc64a35b4b98394c628
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-02 17:09:37 +1000

    sourced

---
+++ R/Stat_ammonium.R
@@ -0,0 +1,93 @@
+###########
+# Shallow #
+###########
+range(lys$nh[lys$depth == "shallow"])
+
+bxplts(value = "nh", ofst= 0.0002, data = subset(lys, depth == "shallow"))
+  # log is slightly better, but homogeneity of variabne 
+  # is violated
+
+# different random factor structure
+m1 <- lme(log(nh + .0002) ~ temp * time, random = ~1|chamber/location, subset = depth == "shallow", data = lys)
+m2 <- lme(log(nh + .0002) ~ temp * time, random = ~1|chamber, subset = depth == "shallow", data = lys)
+m3 <- lme(log(nh + .0002) ~ temp * time, random = ~1|id, subset = depth == "shallow", data = lys)
+anova(m1, m2, m3)
+  # m3 is slightly better
+
+# autocorrelation
+atcr.cmpr(m3, rndmFac= "id")
+  # model4 is best
+
+atml <- atcr.cmpr(m3, rndmFac= "id")[[4]]
+
+Anova(atml)
+
+# model simplification
+MdlSmpl(atml)
+  # interaction of temp x time and temp are removable
+
+Fml <- MdlSmpl(atml)$model.reml
+
+# the final model is
+lme(log(nh + .0002) ~ time, random = ~1|id,
+    correlation=corAR1(),
+    subset = depth == "shallow", data = lys)
+
+Anova(Fml)
+
+summary(Fml)
+
+plot(allEffects(Fml))
+
+# model diagnosis
+plot(Fml)
+qqnorm(Fml, ~ resid(.)|id)
+qqnorm(residuals.lm(Fml))
+qqline(residuals.lm(Fml))
+  # not great....
+
+########
+# Deep #
+########
+range(lys$nh[lys$depth == "deep"])
+
+bxplts(value = "nh", ofst= 0.00271, data = subset(lys, depth == "deep"))
+  #remove the lower outlier
+
+bxplts(value = "nh", ofst= 0.00271, data = subset(lys, depth == "deep" & nh > min(nh)))
+  # improved a lot; log looks better
+
+# different random factor structure
+m1 <- lme(log(nh + .00271) ~ temp * time, random = ~1|chamber/location, subset = depth == "deep", data = lys)
+m2 <- lme(log(nh + .00271) ~ temp * time, random = ~1|chamber, subset = depth == "deep", data = lys)
+m3 <- lme(log(nh + .00271) ~ temp * time, random = ~1|id, subset = depth == "deep", data = lys)
+anova(m1, m2, m3)
+  # m3 is slightly better
+
+# autocorrelation
+atcr.cmpr(m3, rndmFac= "id")
+  # no need for autocorrelation
+
+Anova(m3)
+
+# model simplification
+MdlSmpl(m3)
+  # interaction of temp x time and temp are removable
+
+Fml <- MdlSmpl(m3)$model.reml
+
+# The final model is:
+lme(log(nh + .00271) ~ time, random = ~1|id, subset = depth == "deep", data = lys)
+
+Anova(Fml)
+
+summary(Fml)
+
+plot(allEffects(Fml))
+
+# model diagnosis
+plot(Fml)
+qqnorm(Fml, ~ resid(.)|id)
+qqnorm(residuals.lm(Fml))
+qqline(residuals.lm(Fml))
+  # there are few weird ones....
--- R/Stats.R
+++ R/Stats.R
@@ -2,3 +2,8 @@
 # Nitrate #
 ###########
 source("R//Stat_nitrate.R")
+
+############
+# Ammonium #
+############
+source("R//Stat_ammonium.R")
