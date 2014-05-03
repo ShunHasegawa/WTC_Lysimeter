abfd5b8a15a1f776bcfcadab15908fdf948deb0e
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-03 14:21:43 +1000

    add INVERSE in boxplot function

--- R/functions.R
+++ R/functions.R
@@ -307,14 +307,15 @@ atcr.cmpr <- function(model, rndmFac){
 ###########################################
 # produce box plots with transformed data #
 ###########################################
-# log OR sqrt OR power(1/3)
+# log OR sqrt OR power(1/3) OR inverse
 bxplts <- function(value, ofst = 0, data){
-  par(mfrow = c(2,2))
+  par(mfrow = c(2, 3))
   y <- data[[value]] + ofst #ofst is added to make y >0
   boxplot(y ~ temp*time, data)
   boxplot(log(y) ~ temp*time, main = "log", data)
   boxplot(sqrt(y) ~ temp*time, main = "sqrt", data)
   boxplot(y^(1/3) ~ temp*time, main = "power(1/3)", data)
+  boxplot(1/y ~ temp*time, main = "inverse", data)
   par(mfrow = c(1,1))
 }
 
