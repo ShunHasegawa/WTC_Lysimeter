888735a94ba12af996693838e9e92d05882d14c1
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-05-03 12:36:12 +1000

    homogeneity of variabnce is violated

--- R/Stat_tc.R
+++ R/Stat_tc.R
@@ -1,29 +1,26 @@
 ###########
 # Shallow #
 ###########
-range(lys$toc[lys$depth == "shallow"], na.rm = TRUE)
-boxplot(lys$toc)
-bxplts(value = "toc", ofst = 1, data = subset(lys, depth == "shallow"))
+range(lys$tc[lys$depth == "shallow"], na.rm = TRUE)
+bxplts(value = "tc", ofst = 1, data = subset(lys, depth == "shallow"))
 
 # remove lower outlier
-bxplts(value = "toc", ofst = 1, data = subset(lys, depth == "shallow" & toc > min(toc, na.rm = TRUE)))
-# log looks slightly better, but homogeneity of variance is violated
-
-tocRmOl <- subset(lys, toc > min(toc, na.rm = TRUE))
+bxplts(value = "tc", ofst = 1, data = subset(lys, depth == "shallow" & tc != min(tc, na.rm = TRUE)))
+  # log looks slightly better, but homogeneity of variance is violated
 
 # different random factor structure
-m1 <- lme(log(toc + 1) ~ temp * time, random = ~1|chamber/location, subset = depth == "shallow", 
-          data = tocRmOl, na.action = "na.omit")
-m2 <- lme(log(toc + 1) ~ temp * time, random = ~1|chamber, subset = depth == "shallow", 
-          data = tocRmOl, na.action = "na.omit")
-m3 <- lme(log(toc + 1) ~ temp * time, random = ~1|id, subset = depth == "shallow", 
-          data = tocRmOl, na.action = "na.omit")
+m1 <- lme(log(tc + 1) ~ temp * time, random = ~1|chamber/location, subset = depth == "shallow", 
+          data = tcRmOl, na.action = "na.omit")
+m2 <- lme(log(tc + 1) ~ temp * time, random = ~1|chamber, subset = depth == "shallow", 
+          data = tcRmOl, na.action = "na.omit")
+m3 <- lme(log(tc + 1) ~ temp * time, random = ~1|id, subset = depth == "shallow", 
+          data = tcRmOl, na.action = "na.omit")
 anova(m1, m2, m3)
-# m3 is slightly better
+  # m3 is slightly better
 
-# autocorrelation
+# autcorrelation
 atcr.cmpr(m3, rndmFac= "id")
-# model4 is best
+  # model4 is best
 
 atml <- atcr.cmpr(m3, rndmFac= "id")[[4]]
 
@@ -31,16 +28,16 @@ Anova(atml)
 
 # model simplification
 MdlSmpl(atml)
-# interaction of temp x time; 
-# temp remained yet not significant
+  # interaction of temp x time; 
+  # temp remained yet not significant
 
 Fml <- MdlSmpl(atml)$model.reml
 
 # the final model is:
-lme(log(toc + 1) ~ temp + time, random = ~1|id, 
+lme(log(tc + 1) ~ temp + time, random = ~1|id, 
     subset = depth == "shallow", 
     correlation=corAR1(),
-    data = tocRmOl, na.action = "na.omit")
+    data = tcRmOl, na.action = "na.omit")
 
 Anova(Fml)
 
@@ -50,35 +47,35 @@ plot(allEffects(Fml))
 
 # model diagnosis
 plot(Fml)
-# wedge-shaped: homogeity of variance is not met
+  # wedge-shaped: homogeity of variance is not met
 qqnorm(Fml, ~ resid(.)|id)
 qqnorm(residuals.lm(Fml))
 qqline(residuals.lm(Fml))
 
-# if not using autocorrelation
+# if not using autcorrelation
 Anova(MdlSmpl(m3)$model.reml)
 plot(MdlSmpl(m3)$model.reml)
-# but even more wedged...
+  # but even more wedged...
 
 
 ########
 # Deep #
 ########
-range(lys$toc[lys$depth == "deep"], na.rm = TRUE)
-bxplts(value = "toc", data = subset(lys, depth == "deep"))
+range(lys$tc[lys$depth == "deep"], na.rm = TRUE)
+bxplts(value = "tc", data = subset(lys, depth == "deep"))
 # log looks better
-
+  
 # different random factor structure
-m1 <- lme(log(toc) ~ temp * time, random = ~1|chamber/location, subset = depth == "deep", 
+m1 <- lme(log(tc) ~ temp * time, random = ~1|chamber/location, subset = depth == "deep", 
           data = lys, na.action = "na.omit")
-m2 <- lme(log(toc) ~ temp * time, random = ~1|chamber, subset = depth == "deep", 
+m2 <- lme(log(tc) ~ temp * time, random = ~1|chamber, subset = depth == "deep", 
           data = lys, na.action = "na.omit")
-m3 <- lme(log(toc) ~ temp * time, random = ~1|id, subset = depth == "deep", 
+m3 <- lme(log(tc) ~ temp * time, random = ~1|id, subset = depth == "deep", 
           data = lys, na.action = "na.omit")
 anova(m1, m2, m3)
 # m1 is slightly better
 
-# autocorrelation
+# autcorrelation
 atcr.cmpr(m1, rndmFac= "chamber/location")
 # model4 is best
 
@@ -93,7 +90,7 @@ MdlSmpl(atml)
 Fml <- MdlSmpl(atml)$model.reml
 
 # the final model is:
-lme(log(toc) ~ time, 
+lme(log(tc) ~ time, 
     random = ~1|chamber/location,
     correlation=corAR1(),
     subset = depth == "deep", 
