425402280d26ae8137e128b17e199017649b8d44
ShunHasegawa <shun.hasegawa10@imperial.ac.uk>
2015-07-23 11:01:47 +0100

    merge with soil variables

--- R/Stats.R
+++ R/Stats.R
@@ -1,12 +1,54 @@
+#############################
+# Merge with soil variables #
+#############################
+diff(unique(lys$date))
+# use 1month(28 days) mean
+
+# soil variables
+load("Data/WTC_soilMoistTemp_Chamber_DailySummary.RData")
+# restructure
+names(soilChmSmry)[c(1, 2, 4)] <- c("date", "chamber", "probe")
+SoilChMlt <- melt(soilChmSmry, id = c("date", "chamber", "temp", "probe"))
+SoilCh <- cast(SoilChMlt, date + chamber + temp ~ probe + variable)
+
+# mean of soil vars during incubation period
+SoilIncSampMean <- function(insertion, sampling, Chm, data = SoilCh){
+  a <- subset(data, date >= insertion & date <= sampling & chamber == Chm)
+  vars <- names(a)[which(!names(a) %in% c("date", "chamber", "temp"))]
+  b <- ddply(a, .(chamber), function(x) colMeans(x[, vars], na.rm = TRUE))
+  return(cbind(insertion, sampling, b))
+}
+
+
 # chamber mean
 head(lys)
-Lys_DF <- ddply(lys, .(date, time, chamber, depth, temp), 
-                function(x) colMeans(x[, c("no", "po", "nh", "toc", "tc", "ic", "tn")], 
-                                     na.rm = TRUE))
+Lys_ChMean <- ddply(lys, .(date, time, chamber, depth, temp),  
+                    function(x) colMeans(x[, c("no", "po", "nh", "toc", "tc", "ic", "tn")], 
+                                         na.rm = TRUE))
+
+
+# compute means of soil variabes for each incubation period (28 days), and merge
+# with nutrient df
+names(Lys_ChMean)
+Lys_DF <- ddply(Lys_ChMean, .(time, date, chamber, depth, temp, no, nh, po, toc, tc, ic),
+                 function(x) SoilIncSampMean(insertion= x$date - 14, sampling= x$date,
+                                             Chm = x$chamber))
+Lys_DF$moist <- Lys_DF$SoilVW_5_25_Mean
 
 # df for shallow and deep
 Sdf <- subsetD(Lys_DF, depth == "shallow")
 Ddf <- subsetD(Lys_DF, depth == "deep")
+
+# visudally check if the function works
+p <- ggplot(SoilCh, aes(x = date, y = SoilVW_5_25_Mean))
+p2 <- p + 
+  geom_line() +
+  geom_point(data = Ddf, aes(x = date - 14, y = SoilVW_5_25_Mean), 
+             col = "red", size = 2)+
+  facet_wrap( ~ chamber)+
+  geom_vline(xintercept = as.numeric(unique(Ddf$date)), linetype = "dashed") +
+  geom_vline(xintercept = as.numeric(unique(Ddf$date)) - 28, linetype = "dashed")
+p2  
 
 # df without time1 (pre-plant installation)
 Sdf2 <- subsetD(Sdf, time != 1)
